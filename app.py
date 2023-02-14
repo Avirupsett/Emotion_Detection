30baf8f275098a69bca86075f2ace34355aef029
Avirup Sett <settavirup@gmail.com>
2023-02-14 21:42:51 +0530

    second

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,jsonify,request
 import cv2
 from deepface import DeepFace
+from PIL import Image
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -12,8 +13,9 @@ def home():
 @app.route("/process",methods=['POST'])
 def process_img():
     file=request.files['image']
-    img=cv2.imread(file.stream)
-    prediction=DeepFace.analyze(img,actions = ['emotion'])
+    img=Image.open(file.stream)
+    full_img=cv2.imread(img)
+    prediction=DeepFace.analyze(full_img,actions = ['emotion'])
     return jsonify({'emotion':prediction['dominant_emotion']})
 
 
